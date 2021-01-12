fd4b072c32a0289ee26eb1b18a74783a3c3dcada
Rasmus Jürs <rasmus@jurs.me>
2021-01-12 11:02:09 +0100

    Add search field placeholder

--- pages/index.vue
+++ pages/index.vue
@@ -17,6 +17,7 @@
               v-model="search"
               type="text"
               name="search"
+              placeholder="Søg efter varenummer"
               class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
             />
           </div>
